163e5f7e53ba540ccde4fbebccb8092c9fa0f1e3
supergaojian <4002853745@qq.com>
2019-05-11 11:36:59 +0800

    update test java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,8 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+    String str= "Junit is working fine";
+    Assert.assertEquals("Junit is working fine",str);
 	}
 
 }
